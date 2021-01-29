20eed917174099da4e3f12599a748a8a39bfc12d
Kevin Klukowski <kklukowski12@gmail.com>
2021-01-28 19:34:56 -0500

    add testing

--- useronboarding/cypress/integration/form_test.js
+++ useronboarding/cypress/integration/form_test.js
@@ -10,11 +10,38 @@ describe('signup page', () => {
     const passInput = () => cy.get('input[name="password"]');
     const emailInput = () => cy.get('input[name="email"]');
     const userInput = () => cy.get('input[name="first_name"]');
+    const checked = () => cy.get('input[type="checkbox"]');
+    
 
-    it('can type inputs', () => {
+    it('can type a username', () => {
         userInput()
         .should('have.value', '')
         .type('CatPerson99')
         .should('have.value', 'CatPerson99')
+    });
+
+    it('can type an email', () => {
+        emailInput()
+        .should('have.value', '')
+        .type('kklukowski@gmail.com')
+        .should('have.value', 'kklukowski@gmail.com')
+    });
+
+    it('can type a password', () => {
+        passInput()
+        .should('have.value', '')
+        .type('catsarecool37', { delay: 100 })
+        .should('have.value', 'catsarecool37')
+    });
+
+    it('can accept the terms', () => {
+        checked()
+        .check()
+    });
+
+    it('can submit the form', () => {
+        cy.get('#button')
+        .click()
+
     })
 })
